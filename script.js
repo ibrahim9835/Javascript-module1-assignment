75414bb8f339b015e667e0607d697448c3ec7958
Ibrahim <ibrahimismailcc101@gmail.com>
2025-10-31 20:39:53 +0000

    initialized git

---
+++ script.js
@@ -0,0 +1,8 @@
+window.alert("Welcome to my website");
+
+function paragraph() {
+    document.getElementById("info").innerHTML="You just clicked the button!";
+};
+console.log("Ibrahim Ismail");
+console.log("Blue");
+console.log(2025);
